8ec89a69c9c42445086c33e74905f18d75330fad
Jean Henrique <henrique.jeanschvartz@gmail.com>
2024-05-13 21:28:01 -0300

    Alterando estrutura

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,7 +1,6 @@
-const { mongoUri } = require('../.env')
 const mongoose = require('mongoose')
 
-mongoose.connect(mongoUri, {
+mongoose.connect(process.env.MONGO_URI || '', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
