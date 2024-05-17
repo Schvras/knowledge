e9fd6a12e5a819f6c793f3d1a63160af7cc2656c
Jean Henrique <henrique.jeanschvartz@gmail.com>
2024-05-16 21:33:46 -0300

    Alter

--- api/app.js
+++ api/app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express()
 const PORT = 8000
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.get('/webhook', (req, res) => {
+  res.send('123456')
 })
 
 app.get('/about', (req, res) => {
